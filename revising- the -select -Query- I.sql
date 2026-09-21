32e8d03bae7899c2a2c66c01ae8996c0f64099e0
sangluu834-maker <sangluu834@gmail.com>
2026-09-21 21:24:58 +0700

    Add SQL query for American cities with large populations
    Added a SQL query to retrieve all columns for American cities with populations over 100,000 from the CITY table.

---
+++ revising- the -select -Query- I.sql
@@ -0,0 +1,23 @@
+# Revising the Select Query I
+
+## 1. Đề bài
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+The CITY table is described as follows:
+
+## 2. Bảng CITY
+
+| Field | Type |
+|---|---|
+| ID | NUMBER |
+| NAME | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3) |
+| DISTRICT | VARCHAR2(20) |
+| POPULATION | NUMBER |
+
+## 3. SQL
+
+```sql
+SELECT *
+FROM CITY
+WHERE POPULATION > 100000
+  AND COUNTRYCODE = 'USA';
